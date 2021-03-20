3546e0856ce332452376d7b4ea98aea1813e9aa7
Kapil <ykapil307@gmail.com>
2021-03-20 12:12:36 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BookListComponent} from './book-list/book-list.component';
+import{AddBookComponent} from './add-book/add-book.component';
+import {UpdateBookComponent} from './update-book/update-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add-book',component:AddBookComponent},
+  {path:'',redirectTo:'add-book',pathMatch:'full'},
+  {path:'book-list',component:BookListComponent},
+  {path:'update-book/:id',component:UpdateBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
